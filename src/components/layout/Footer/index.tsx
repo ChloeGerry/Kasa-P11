4b7d44ad9704ffb5a985b43e3dbc909c2b10da16
Chloé Gerry <cgerry@corp.free.fr>
2024-08-23 13:41:14 +0200

    feat: add footer

---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,12 @@
+import FooterLogo from "../Logo/FooterLogo";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <FooterLogo />
+      <p className="footer__copyright">© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/layout/Header/index.tsx";
+import Footer from "./components/layout/Footer/index.tsx";
 import Routing from "./Routing.tsx";
 import "./style/index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Header />
       <Routing />
+      <Header />
+      <Footer />
     </BrowserRouter>
   </StrictMode>
 );
