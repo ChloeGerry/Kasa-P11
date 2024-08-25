07cc6e8f0055e49d35bcfa5e0afe3ffe8000ed03
Chloé Gerry <cgerry@corp.free.fr>
2024-08-25 23:20:54 +0200

    chore: add vite config path to avoid complicated imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import sass from "sass";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
       scss: {
         implementation: sass,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
     },
   },
 });
