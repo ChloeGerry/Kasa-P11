b941e8d585317ba5a6e225c3d17eb6362f42d0bb
Chloé Gerry <cgerry@corp.free.fr>
2024-09-04 11:33:33 +0200

    feat: create about page banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,19 +2,28 @@ type BannerProps = {
   title?: string;
   image: string;
   margin: "large" | "medium";
+  filter: "enlighten" | "darken";
 };
 
-const Banner = ({ title, image, margin }: BannerProps) => (
-  <section
-    className={`banner ${
-      margin === "large" ? "banner__section--large" : "banner__section--medium"
-    }`}
-  >
-    <div className="banner__wrapper">
-      <img src={image} alt="banner" className="banner__image" />
-      {title && <h1 className="banner__title">{title}</h1>}
-    </div>
-  </section>
-);
+const Banner = ({ title, image, margin, filter }: BannerProps) => {
+  return (
+    <section
+      className={`banner ${
+        margin === "large" ? "banner__section--large" : "banner__section--medium"
+      }`}
+    >
+      <div className="banner__wrapper">
+        <img
+          src={image}
+          alt="banner"
+          className={`banner__image ${
+            filter === "darken" ? "banner__image--darken" : "banner__image--enlighten"
+          }`}
+        />
+        {title && <h1 className="banner__title">{title}</h1>}
+      </div>
+    </section>
+  );
+};
 
 export default Banner;
