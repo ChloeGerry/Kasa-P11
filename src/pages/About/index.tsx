9cc100c07d4a64c56b25e883f0c85879f5972382
Chloé Gerry <cgerry@corp.free.fr>
2024-09-04 11:47:46 +0200

    feat: create about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,19 @@
+import Banner from "@/components/Banner";
+import Collapse from "@/components/Collapse";
+import { additionnalInformations } from "@/services/additionnalInformations";
+import bannerImage from "@/assets/about-banner.jpg";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main>
+      <Banner margin="medium" image={bannerImage} filter="enlighten" />
+      <section className="collapses__wrapper">
+        {additionnalInformations.map(({ size, title, text }) => {
+          return <Collapse key={title} size={size} title={title} text={text} />;
+        })}
+      </section>
+    </main>
+  );
 };
 
 export default About;
