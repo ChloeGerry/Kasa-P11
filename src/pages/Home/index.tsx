67e997cfdd11367b46b1776c015bba64ba61391b
Chloé Gerry <cgerry@corp.free.fr>
2024-09-04 11:40:44 +0200

    style: update home page style props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,20 @@
 import Banner from "@/components/Banner";
 import Cards from "@/components/Cards";
 import bannerImage from "@/assets/homepage-banner.jpg";
+import { useHousings } from "@/hooks/useHousings";
 
 const Home = () => {
+  const housings = useHousings();
+
   return (
     <main>
-      <Banner margin="large" title="Chez vous, partout et ailleurs" image={bannerImage} />
-      <Cards />
+      <Banner
+        margin="large"
+        title="Chez vous, partout et ailleurs"
+        image={bannerImage}
+        filter="darken"
+      />
+      <Cards housings={housings} />
     </main>
   );
 };
