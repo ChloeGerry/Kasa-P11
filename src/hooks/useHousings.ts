1a246454016cfdd02a72680e6fbeca8677e39817
Chloé Gerry <cgerry@corp.free.fr>
2024-09-06 19:13:46 +0200

    fix: hook fetch route import

--- src/hooks/useHousings.ts
+++ src/hooks/useHousings.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import { Housing } from "./types";
+import { HousingType } from "./types";
 
 export const useHousings = () => {
-  const [housings, setHousings] = useState<Housing[] | null>([]);
+  const [housings, setHousings] = useState<HousingType[] | null>([]);
   const getHousings = async (): Promise<void> => {
     try {
-      const result = await fetch("housings.json");
+      const result = await fetch("/housings.json");
       const currentHousings = await result.json();
 
       if (!currentHousings) {
