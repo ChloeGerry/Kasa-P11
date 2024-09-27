1ff715c03ce0e8d12de322e171290704882c3895
Chloé Gerry <cgerry@corp.free.fr>
2024-09-27 09:12:58 +0200

    refactor: update housing type

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -2,4 +2,14 @@ export type HousingType = {
   id: string;
   title: string;
   cover: string;
+  pictures?: string[];
+  description?: string;
+  host?: {
+    name: string;
+    picture: string;
+  };
+  rating?: string;
+  location?: string;
+  equipments?: string[];
+  tags?: string[];
 };
