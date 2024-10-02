84f982d36104900ab60e51e2dd21d05166376527
Chloé Gerry <cgerry@corp.free.fr>
2024-10-02 19:54:17 +0200

    refactor: callout component to make it less specific

--- src/components/Callout/index.tsx
+++ src/components/Callout/index.tsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 type CalloutProps = {
   text: string;
   hasRedirection: boolean;
+  color: string;
+  linkText?: string;
 };
 
-const Callout = ({ text, hasRedirection }: CalloutProps) => {
+const Callout = ({ text, hasRedirection, color, linkText }: CalloutProps) => {
   return (
-    <div className="callout">
+    <div className="callout" color={color} style={{ backgroundColor: color }}>
       <p className="callout__text">{text}</p>
       {hasRedirection && (
         <Link to="/" className="callout__link">
-          Retourner sur la page d'accueil
+          {linkText}
         </Link>
       )}
     </div>
